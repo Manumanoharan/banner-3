788219c1a442f434116b0c5ebc1c46bdc3a519d6
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-07-05 11:35:45 +0530

    class

--- banner-3/js/main.js
+++ banner-3/js/main.js
@@ -1,43 +1,53 @@
-function startAD(){
-    setTimeout(function(){
-        document.getElementById('bg-img').style.left='-222px';
-        document.getElementById('bg-img').style.transition='all 15s';
-    },800)
-    setTimeout(function(){
-        document.getElementById('blue-1').style.top='-40px';
-        document.getElementById('blue-1').style.transition='all 0.5s';
+function startAd(){
+    setTimeout (function(){
+        document.getElementById('bg-color1').style.transform='translateX(-364px)'
+        document.getElementById('bg-color2').style.transform='translateX(364px)'
+        document.getElementById('bg-color1').style.transition='all 1s ease'
+        document.getElementById('bg-color2').style.transition='all 1s ease'
+
     },200)
-    setTimeout(function(){
-        document.getElementById('blue-2').style.bottom='-43px';
-        document.getElementById('blue-2').style.transition='all 0.5s';
-    },200)
-    setTimeout(function(){
-        document.getElementById("text-1").classList.add("text");
+    setTimeout (function(){
+        document.getElementById('text-1').style.opacity='1'
+        document.getElementById('text-1').style.right='149px'
+        document.getElementById('text-1').style.transition='all 1s ease'
     },500)
-    setTimeout(function(){
-        document.getElementById("text-2").classList.add("text1");
+    setTimeout (function(){
+        document.getElementById('text-2').style.opacity='1'
+        document.getElementById('text-2').style.right='143px'
+        document.getElementById('text-2').style.transition='all 1s ease'
     },700)
-    setTimeout(function(){
-        document.getElementById('text-3').style.opacity="1";
-        document.getElementById('text-3').style.transition='all 1s';
-    },1400)
-    setTimeout(function(){
-        document.getElementById('btn').style.width="111px";
-        document.getElementById('cta').style.transform="scale(1.1)";
-        // document.getElementById('id').style.transform="scale(1.1)";
-        document.getElementById('btn').style.transition='all 0.3s';
-        document.getElementById('cta').style.transition='all 0.3s';
-        // document.getElementById('id').style.transition='all 0.3s';
+    setTimeout (function(){
+        document.getElementById('text-3').style.opacity='1'
+        document.getElementById('text-3').style.bottom='11px'
+        document.getElementById('text-3').style.transition='all 1s ease'
+    },1000)
+    setTimeout (function(){
+        document.getElementById('logo').style.opacity='1'
+        document.getElementById('logo').style.top='22px'
+        document.getElementById('logo').style.transition='all 0.3s ease'
+    },1300)
+    setTimeout (function(){
+        document.getElementById('icon1').style.transform='scale(1)'
+        document.getElementById('icon1').style.transition='all 0.7s ease'
+    },1500)
+    setTimeout (function(){
+        document.getElementById('icon2').style.transform='scale(1)'
+        document.getElementById('icon2').style.transition='all 0.7s ease'
+    },1700)
+    setTimeout (function(){
+        document.getElementById('icon3').style.transform='scale(1)'
+        document.getElementById('icon3').style.transition='all 0.7s ease'
+    },1800)
+    setTimeout (function(){
+        document.getElementById('icon4').style.transform='scale(1)'
+        document.getElementById('icon4').style.transition='all 0.7s ease'
+    },1900)
+    setTimeout (function(){
+        document.getElementById('cta').style.opacity='1'
+        document.getElementById('cta').style.transform='scale(1)'
+        document.getElementById('cta').style.transition='all 1.5s '
     },2000)
-
-    setTimeout(function(){
-        document.getElementById('site').style.transform='translateY(0)';
-        document.getElementById('site').style.transition='all 0.6s';
-    },2200)
-    setTimeout(function(){
-        document.getElementById('logo').style.transform='translateY(0)';
-        document.getElementById('logo').style.transition='all 0.5s';
-    },2700)
-
+   
 }
-window.load=startAD();
+
+window.load=startAd();
